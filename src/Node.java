ac6b85a2c0596cd2f0650b47048e1454923aa259
s3977794 <s3977794@rmit.edu.vn>
2024-04-25 17:14:46 +0700

    Linkedlist

---
+++ src/Node.java
@@ -0,0 +1,9 @@
+class Node {
+    Place data;
+    Node next;
+
+    public Node(Place data) {
+        this.data = data;
+        this.next = null;
+    }
+}
---
+++ src/Place.java
@@ -0,0 +1,13 @@
+
+import java.util.Set;
+class Place {
+    int x;
+    int y;
+    Set<String> services;
+
+    public Place(int x, int y, Set<String> services) {
+        this.x = x;
+        this.y = y;
+        this.services = services;
+    }
+}
