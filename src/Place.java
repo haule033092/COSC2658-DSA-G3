2ab0fc3a2cd8e9c0416c9c7561c708bc96ee16b2
s3977794 <118870483+s3977794@users.noreply.github.com>
2024-05-09 14:05:31 +0700

    Update Place.java

--- src/Place.java
+++ src/Place.java
@@ -1,4 +1,5 @@
 
+import java.lang.reflect.Constructor;
 import java.util.Set;
 class Place {
     protected int x;
@@ -6,13 +7,14 @@ class Place {
     protected int distance;
     protected Set<String> services;
 
+
+//    Constructors
     public Place(int x, int y, Set<String> services) {
         this.x = x;
         this.y = y;
         this.distance=0;
         this.services = services;
     }
-
     public Place(int x, int y, Set<String> services, int distance) {
         this.x = x;
         this.y = y;
