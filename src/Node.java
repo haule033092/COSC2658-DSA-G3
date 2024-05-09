1075086771289da5d9ee6d2f677a24938cf65f17
s3977794 <118870483+s3977794@users.noreply.github.com>
2024-05-09 14:06:43 +0700

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -1,7 +1,11 @@
+import java.lang.reflect.Constructor;
+
 class Node {
     protected Place data;
     protected Node next;
 
+
+//    Constructor
     public Node(Place data) {
         this.data = data;
         this.next = null;
